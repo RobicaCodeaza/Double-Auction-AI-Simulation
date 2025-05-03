df37bfbd4727a5170908303c0269f058b02f8ab8
Stoica Robert <stoicarobert38@yahoo.com>
2025-05-03 19:27:36 +0300

    Config - Configured TailwindCss

--- Disertatie/eslint.config.js
+++ Disertatie/eslint.config.js
@@ -22,6 +22,11 @@ export default [
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
+    settings: {
+      react: {
+        version: '19.0.0',
+      },
+    },
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
